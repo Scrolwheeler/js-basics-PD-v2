21550fc7dfb28df4725a2545ff512f372c50e0f1
Edgars G <Edgars@gmail>
2025-11-10 16:34:05 +0200

    2 uzd pabeigts

---
+++ coffee-order.js
@@ -0,0 +1,24 @@
+let coffeeSize = "large"
+let addSugar = true
+
+if (coffeeSize == "large", addSugar = true){
+    console.log(2 + 0.20)
+}
+return
+
+if (coffeeSize == "large"){
+    console.log(2)
+}
+if (coffeeSize == "medium"){
+    console.log(1.5)
+}
+if (coffeeSize == "small"){
+    console.log(1)
+}
+if (addSugar == true){
+    console.log(0.20)
+}
+
+
+
+
