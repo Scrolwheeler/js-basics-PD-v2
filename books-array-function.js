79e14c17612d918cedc004580786513d5aa163b7
Edgars G <Edgars@gmail>
2025-11-10 17:57:57 +0200

    meiginaju salabot function

--- books-array-function.js
+++ books-array-function.js
@@ -1,13 +1,16 @@
+
+
 let books = [
     {title: "Harijs potters", author: "Dž. K. Roulinga", isAvailable: "Jā"},
     {title: "Sprīdītis", author: "Anna Brigadere", isAvailable: "Nē"}
 ]
 
-function printBookInfo(attribute){
-for(let i = 0; i < 2; i++){
-   console.log(attribute); 
+function printBookInfo(attribute,){
+    for(let i = 0; i < books.length; i++){
+        attribute = books[i]
+console.log(attribute);
 }
 }
+printBookInfo(title);
+printBookInfo(author);
 
-printBookInfo(books[i].title);
-printBookInfo(books[i].author);
